b90cfa9925e0ccb643e9346228dec43a78fec682
トモアキ <h.tkaz15@gmail.com>
2019-12-11 16:51:51 +0900

    third commit

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ function toggleMenu() {
 
         //       Set Menu State
         showMenu = true;
+        
     } else {
         menuBtn.classList.remove("close");
         menu.classList.remove("show");
